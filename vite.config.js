c32205bab3a634e7f24da1b40af909e61f9cf056
Radha Krishna <pradhakrishna594@gmail.com>
2026-02-19 03:12:30 +0530

    Deployment Final: Prepared /docs folder for GitHub Pages and synced production build

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Full-Stack-LAB2/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  }
 })
